bc2ca32bda9b9bc7c651e51e7a3e9d9ba9b59d8e
Nelson <106107189+NelsonAkhigbe@users.noreply.github.com>
2025-01-30 19:14:55 +0100

    Add files via upload

---
+++ FORT.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', () => {
+  alert("Yay! You've made me the happiest person today😂, Fortune!. ❤️");
+});
+
+document.getElementById('noButton').addEventListener('click', () => {
+  alert("Hmm, I'll keep asking until you say yes!😂🤲🏾, Fortune. 💕");
+});
